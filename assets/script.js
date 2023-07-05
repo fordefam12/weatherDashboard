f07e1d7f4ece712ef902cf98a72799ce6ad5025d
fordefam12 <136336831+fordefam12@users.noreply.github.com>
2023-07-05 08:44:03 -0500

    api key input

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,51 @@
 $(document).ready(function () {
-var APIkey = "4910a1ed2efe6e5a111856cd7c08b4de";
-var city; 
-var now = moment().format("1");
-var day1 =moment().add(1, "days").format("1");
-})
+  var APIkey = "4910a1ed2efe6e5a111856cd7c08b4de";
+  var city;
+  var now = moment().format("1");
+  var day1 = moment().add(1, "days").format("1");
+  var day2 = moment().add(2, "days").format("1");
+  var day3 = moment().add(3, "days").format("1");
+  var day4 = moment().add(4, "days").format("1");
+  var day5 = moment().add(5, "days").format("1");
+  var cities;
+
+  function MostRecent() {
+    var lastSearch = localStorage.getItem("mosrRecent");
+    if (lastSearch) {
+      city = lastSearch;
+      search();
+    } else {
+      city = "seatle";
+      search();
+    }
+  }
+  MostRecent();
+
+  function loadRecentCities() {
+    var recentCities = JSON.parse(localStorage.getItem("cities"));
+    if (recentCities) {
+        cities = recentCities;
+    }
+    else {
+        cities = [];
+    }
+  }
+  loadRecentCities()
+  $(".submit").on("click", (e) => {
+    e.preventDefault();
+    getCity();
+    search();
+    $("cityInput").val("");
+    listCities();
+
+  })
+  function search() {
+    var url= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=4910a1ed2efe6e5a111856cd7c08b4de";
+    var coords =[];
+    $.ajax({
+        method: "GET",
+    }).then(function(response){
+        
+    })
+  }
+});
